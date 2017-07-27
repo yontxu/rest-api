7f422000cdda16212cc63ef99772f2d6a6d9165e
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 10:23:32 +0200

    Add simple entity validation

--- symfony-rest-api/src/AppBundle/Controller/CarsController.php
+++ symfony-rest-api/src/AppBundle/Controller/CarsController.php
@@ -11,6 +11,8 @@
 
 	use FOS\RestBundle\Controller\FOSRestController;
 	use FOS\RestBundle\Controller\Annotations\Get;
+	use Symfony\Component\HttpFoundation\Response;
+	use FOS\RestBundle\View\View;
 
 	class CarsController extends FOSRestController {
 
@@ -20,6 +22,9 @@
 		 */
 		public function indexAction() {
 			$cars = $this->getDoctrine()->getRepository( 'AppBundle:Car' )->findAll();
+			if ( $cars === null || empty($cars)) {
+				return new View( "Not found.", Response::HTTP_NOT_FOUND );
+			}
 			return $cars;
 		}
 
@@ -29,6 +34,9 @@
 		 */
 		public function showAction( $id ) {
 			$car = $this->getDoctrine()->getRepository( 'AppBundle:Car' )->find( $id );
+			if ( $car === null) {
+				return new View( "Not found.", Response::HTTP_NOT_FOUND );
+			}
 			return $car;
 		}
 
