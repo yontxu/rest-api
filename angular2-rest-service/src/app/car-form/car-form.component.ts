9797bb227802f96980b41a7cbe5c286051ed52f6
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 15:25:54 +0200

    Define form properties, base validation and populate entity if car_id params is provided

--- angular2-rest-service/src/app/car-form/car-form.component.ts
+++ angular2-rest-service/src/app/car-form/car-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CarsService } from 'app/services/cars.service';
+import { Car } from 'app/models/car';
 
 @Component({
   selector: 'app-car-form',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-form.component.css']
 })
 export class CarFormComponent implements OnInit {
+  carForm: FormGroup;
+  car: Car;
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+      private formBuilder: FormBuilder,
+      private carsService: CarsService) {
 
-  ngOnInit() {
+      this.carForm = this.formBuilder.group({
+        id: [''],
+        model: ['', Validators.required],
+        wheels: ['', Validators.required],
+        color: ['', Validators.required],
+        price: ['', Validators.required]
+      });
   }
 
+  ngOnInit() {
+      this.route.params.subscribe((params: Params) => {
+        let carId = params['car_id'];
+        if (carId) {
+          this.carsService.getCar(carId).subscribe( car => {
+            this.carForm.setValue(car);
+            this.car = car;
+          });
+        }else{
+          this.car = new Car();
+        }
+      });
+  }
+
+  save() {
+    if(this.car.id){
+      console.log('Update');
+    }else{
+      console.log('Create');
+    }
+  }
 }
