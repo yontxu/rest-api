1330aa004bc84cb7ee42f6ad374a506ffee9fb2f
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 09:38:54 +0200

    New Car entity created

---
+++ symfony-rest-api/src/AppBundle/Entity/Car.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Car
+ *
+ * @ORM\Table(name="car")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CarRepository")
+ */
+class Car
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="model", type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="wheels", type="smallint")
+     */
+    private $wheels;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="color", type="string", length=10)
+     */
+    private $color;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set model
+     *
+     * @param string $model
+     *
+     * @return Car
+     */
+    public function setModel($model)
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    /**
+     * Get model
+     *
+     * @return string
+     */
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    /**
+     * Set wheels
+     *
+     * @param integer $wheels
+     *
+     * @return Car
+     */
+    public function setWheels($wheels)
+    {
+        $this->wheels = $wheels;
+
+        return $this;
+    }
+
+    /**
+     * Get wheels
+     *
+     * @return int
+     */
+    public function getWheels()
+    {
+        return $this->wheels;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Car
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Car
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
