b99cba1d35f4b67c1854826bdd8b1104faa62b04
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 09:49:44 +0200

    Create new controller for Car resources and extend FOSRestController

---
+++ symfony-rest-api/src/AppBundle/Controller/CarsController.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * AppBundle\Controller\CarsController
+	 *
+	 * User: yzubizarreta
+	 * Date: 27/7/17
+	 */
+
+	namespace AppBundle\Controller;
+
+
+	use FOS\RestBundle\Controller\FOSRestController;
+
+	class CarsController extends FOSRestController {
+
+	}
