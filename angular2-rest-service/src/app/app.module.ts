0c1595e8b97e1148a5c22734633ffe11ecda880c
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 13:45:05 +0200

    Define route for /cars

--- angular2-rest-service/src/app/app.module.ts
+++ angular2-rest-service/src/app/app.module.ts
@@ -3,17 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+
+const carsRoutes: Routes = [
+  { path: 'cars',
+    component: CarsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot( carsRoutes )
   ],
   providers: [],
   bootstrap: [AppComponent]
