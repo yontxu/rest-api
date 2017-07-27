2753e228c609a0f0ae73e3980e7e4658b825ad64
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 10:33:59 +0200

    Implement the update (PATCH) action for Car entity

--- symfony-rest-api/src/AppBundle/Controller/CarsController.php
+++ symfony-rest-api/src/AppBundle/Controller/CarsController.php
@@ -12,6 +12,7 @@
 	use FOS\RestBundle\Controller\FOSRestController;
 	use FOS\RestBundle\Controller\Annotations\Get;
 	use FOS\RestBundle\Controller\Annotations\Post;
+	use FOS\RestBundle\Controller\Annotations\Patch;
 	use Symfony\Component\HttpFoundation\Request;
 	use Symfony\Component\HttpFoundation\Response;
 	use FOS\RestBundle\View\View;
@@ -48,6 +49,37 @@
 		 */
 		public function postAction( Request $request ) {
 			$car = new Car();
+
+			$model = $request->get( 'model' );
+			$wheels = $request->get( 'wheels' );
+			$color  = $request->get( 'color' );
+			$price = $request->get( 'price' );
+
+			//todo: improve request data type validation
+			if ( empty($model) || empty($wheels) || empty($color) || empty($price) ) {
+				return new View("Unable to process the Car entity", Response::HTTP_NOT_ACCEPTABLE);
+			}
+
+			$car->setModel($model);
+			$car->setWheels($wheels);
+			$car->setColor($color);
+			$car->setPrice($price);
+
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($car);
+			$em->flush();
+
+			return $car;
+		}
+
+		/**
+		 * @Patch("/cars/{id}")
+		 */
+		public function updateAction( $id, Request $request ) {
+			$car = $this->getDoctrine()->getRepository( 'AppBundle:Car' )->find( $id );
+			if ( $car === null) {
+				return new View( "Not found.", Response::HTTP_NOT_FOUND );
+			}
 
 			$model = $request->get( 'model' );
 			$wheels = $request->get( 'wheels' );
