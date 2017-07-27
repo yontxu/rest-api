1741d5290ac46c39b5fe252765047be378a78fbc
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 14:14:56 +0200

    Implement CarsService to retrieve car entity based on passed car_id param

--- angular2-rest-service/src/app/car-detail/car-detail.component.ts
+++ angular2-rest-service/src/app/car-detail/car-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CarsService } from 'app/services/cars.service';
 
 @Component({
   selector: 'app-car-detail',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-detail.component.css']
 })
 export class CarDetailComponent implements OnInit {
+  car;
 
-  constructor() { }
+  constructor(
+    private carsService: CarsService,
+    private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.carsService.getCar(params['car_id']).subscribe(p => this.car = p);
+    });
   }
 
 }
