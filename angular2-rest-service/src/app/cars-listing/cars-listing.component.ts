200d506fe3ae5683452c476c31684cb00707e94c
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 14:10:13 +0200

    Import CarsService provider and populate cars property on ngOnInit event

--- angular2-rest-service/src/app/cars-listing/cars-listing.component.ts
+++ angular2-rest-service/src/app/cars-listing/cars-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarsService } from 'app/services/cars.service';
 
 @Component({
   selector: 'app-cars-listing',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-listing.component.css']
 })
 export class CarsListingComponent implements OnInit {
+  cars;
 
-  constructor() { }
+  constructor(private carsService: CarsService) {}
 
   ngOnInit() {
+    this.carsService.getCars().subscribe(p => this.cars = p);
   }
 
 }
