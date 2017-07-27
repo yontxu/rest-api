bc28a921ad457bcefe0664245422ea8431031e4c
Yon Zubizarreta <yonzubi@gmail.com>
2017-07-27 13:39:41 +0200

    Import Bootstrap module

--- angular2-rest-service/src/app/app.module.ts
+++ angular2-rest-service/src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
